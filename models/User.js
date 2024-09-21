341eb3b85293770a5ebd5b9bf88ba1e9fb2d1078
ebel-frank <frankebeledike@gmail.com>
2024-09-21 23:18:40 +0100

    hotfix

--- models/User.js
+++ models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const userScehema = new Schema({
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     pass: {
         type: String,
--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,46 @@
+const argon2 = require('argon2');
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
 router.post('/auth/register', async (req, res) => {
     try {
-        const {email, pass} = req.body
-        const user = new User({email, pass})
+        const { email, pass } = req.body
+
+        const hashedPassword = await argon2.hash(pass);
+        const user = new User({ email, pass: hashedPassword })
         await user.save()
         res.status(201).send()
     } catch (e) {
-        res.status(500).send({ error: 'Internal Server Error' })
+        if (e.code === 11000) {
+            res.status(500).send({ error: "Email already exist" })
+        } else {
+            res.status(500).send({ error: `Error: ${e.code}` })
+        }
     }
 })
 
 router.post('/auth/login', async (req, res) => {
     try {
-        const {email, pass} = req.body
-        const user = await User.findOne({email, pass})
+        const { email, pass } = req.body
+        const user = await User.findOne({ email })
+        console.log(user);
+        console.log(pass);
+        
         if (!user) {
-            res.status(200).json({message: "Invalid credentials", status: false})
+            res.status(200).json({ message: "Invalid credentials", status: false })
         } else {
-            res.status(200).json({message: "Success", status: true})
+            const isMatch = await argon2.verify(user.pass, pass);
+            if (isMatch) {
+                // Passwords match, proceed with login
+                res.status(200).json({ message: "Success", status: true })
+            } else {
+                res.status(200).json({ message: "Invalid credentials", status: false })
+            }
         }
     } catch (e) {
+        console.log(e);
+        
         res.status(500).send({ error: 'Internal Server Error' })
     }
 })
